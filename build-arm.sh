c0064e8d2f55466d67d8ff49fea716b0e1aa10e4
julianortlieb <mygames@julian-ortlieb.de>
2024-12-30 19:21:09 +0100

    Update build-arm.sh to check for aarch64 architecture and automate package installation

--- build-arm.sh
+++ build-arm.sh
@@ -8,7 +8,24 @@ echo "Kernel: $(uname -r)"
 echo "Date: $(date)"
 
 # Verify that the system is running on ARM
-if [ "$(uname -m)" != "armv7l" ]; then
+if [ "$(uname -m)" != "aarch64" ]; then
     echo "This script is intended to run on ARM architecture only."
     exit 1
 fi
+
+# Update the package list
+echo "Updating the package list..."
+sudo apt-get update
+
+cd pve-common
+
+# Install the required packages
+echo "Installing the required packages..."
+yes|mk-build-deps --install
+
+# Build the package
+echo "Building the package..."
+make deb
+
+# Debugging
+ls

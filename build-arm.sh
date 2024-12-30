765682c1e3946243c8f1fdb3de7dbb2bf454d7f2
julianortlieb <mygames@julian-ortlieb.de>
2024-12-30 19:17:20 +0100

    Enhance build-arm.sh with system information and ARM architecture check

--- build-arm.sh
+++ build-arm.sh
@@ -1,1 +1,14 @@
+echo "#################"
 echo "Init build-arm.sh"
+echo "#################"
+echo ""
+echo "Processor: $(uname -m)"
+echo "OS: $(uname -o)"
+echo "Kernel: $(uname -r)"
+echo "Date: $(date)"
+
+# Verify that the system is running on ARM
+if [ "$(uname -m)" != "armv7l" ]; then
+    echo "This script is intended to run on ARM architecture only."
+    exit 1
+fi
